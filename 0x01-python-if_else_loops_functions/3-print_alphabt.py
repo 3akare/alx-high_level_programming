d5f9959a68177af646048844141c5b63cd170f74
bakare <bakaredavid007@gmail.com>
2022-08-23 17:57:28 +0100

    task 3 finally completed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for alpha in range(97, 123):
-    if ((alpha == 101) or (alpha == 113)):
-        print("", end="")
-    else:
+    if ((alpha != 101) and (alpha != 113)):
         print("{}".format(chr(alpha)), end="")
