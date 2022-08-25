749ff21d107f6f4c91df18ee6d16b8911f47b565
bakare <bakaredavid007@gmail.com>
2022-08-25 03:01:00 +0100

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,13 @@
-
+#!/usr/bin/python3
+i = 0
+j = 0
+while (i <= 9):
+    j = i + 1
+    while (j <= 9):
+        if (j == 9 and i == 8):
+            print("{}{}".format(i, j), end="")
+            print("")
+            break
+        print("{}{}".format(i, j), end=", ")        
+        j = j + 1
+    i = i + 1
