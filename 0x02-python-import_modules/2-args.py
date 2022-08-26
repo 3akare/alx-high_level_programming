a0214314927f0bd216bb1778f627b20795f97ab3
bakare <bakaredavid007@gmail.com>
2022-08-26 03:00:47 +0100

    test 3, task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 lenOfArgv = (len(sys.argv) - 1)
 index = 1
 if ((lenOfArgv == 0)):
-    print(f'{lenOfArgv} arguments.')
+    print("{} arguments.".format(lenOfArgv))
 else:
     if (lenOfArgv == 1):
-        print(f'{lenOfArgv} argument:')
-        print(f'{lenOfArgv}: {sys.argv[lenOfArgv]}')
+        print("{} argument:".format(lenOfArgv))
+        print("{}: {}".format(lenOfArgv, sys.argv[lenOfArgv]))
     else:
-        print(f'{lenOfArgv} arguments:')
+        print("{} arguments:". format(lenOfArgv))
         while (index < (lenOfArgv + 1)):
-            print(f'{index}: {sys.argv[index]}')
+            print("{}: {}".format(index, sys.argv[index]))
             index += 1
