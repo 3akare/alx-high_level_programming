efd38edf6946ec60bf60ab17acc448b41870b383
bakare <bakaredavid007@gmail.com>
2022-08-28 00:48:40 +0100

    test 2, task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from turtle import clear
+
+
 def max_integer(my_list=[]):
+    if len(my_list) < 0:
+        return None
     my_list = sorted(my_list)
     Len = len(my_list)
     return my_list[Len - 1]
-
