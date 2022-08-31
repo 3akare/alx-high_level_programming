27ea65080daf107a3d126bf34c9c6cb8ca57dfe3
bakare <bakaredavid007@gmail.com>
2022-08-31 21:11:15 +0100

    test 2, task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -5,6 +5,6 @@ def common_elements(set_1, set_2):
     new_list = []
     for i in range(len(set_1)):
         for j in range(len(set_2)):
-            if  (set_1[i] == set_2[j]):
+            if (set_1[i] == set_2[j]):
                 new_list.append(set_2[j])
     return set(new_list)
