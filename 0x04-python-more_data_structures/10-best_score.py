8c83749be5b4c9a774df8f02a9bab3d9573def66
bakare <bakaredavid007@gmail.com>
2022-08-31 22:03:53 +0100

    test 4, task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        return max(a_dictionary)
-    else:
+    if not a_dictionary:
         return None
+    else:
+        return max(a_dictionary)
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
