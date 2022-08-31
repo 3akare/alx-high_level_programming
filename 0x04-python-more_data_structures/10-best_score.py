f47ad26a61b4c3b163957a5839812d60f65d58ad
bakare <bakaredavid007@gmail.com>
2022-08-31 22:15:55 +0100

    test 7, task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        max_number, position = 0, 0
-        keys_list = list(a_dictionary.keys())
-        val_list = list(a_dictionary.values())
-        for value in a_dictionary.values():
-            if isinstance(value, int):
-                if value > max_number:
-                    max_number = value
-        position = val_list.index(max_number)
-        return keys_list[position]
-    return None
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    max_number, position = 0, 0
+    keys_list = list(a_dictionary.keys())
+    val_list = list(a_dictionary.values())
+    for value in a_dictionary.values():
+        if isinstance(value, int):
+            if value > max_number:
+                max_number = value
+    position = val_list.index(max_number)
+    return keys_list[position]
