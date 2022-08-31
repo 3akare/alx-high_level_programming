80146312166ea4dd71c88cfaec2852808172a06a
bakare <bakaredavid007@gmail.com>
2022-08-31 21:53:43 +0100

    test 2, task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,4 +10,12 @@ def best_score(a_dictionary):
                     max_number = value
         position = val_list.index(max_number)
         return keys_list[position]
-    return None
+    else:
+        return None
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
