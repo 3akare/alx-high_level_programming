1e8969c68941a44ab0ff46275f9f9033116514ab
bakare <bakaredavid007@gmail.com>
2022-08-31 20:19:57 +0100

    test 1, task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for j in range(len(new_list)):
+        for i in range(len(new_list[j])):
+            new_list[j][i] = pow(new_list[j][i], 2)
+    return new_list
