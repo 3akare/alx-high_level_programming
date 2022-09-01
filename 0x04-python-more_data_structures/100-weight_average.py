189a19b95e28f4792e7276d1b5e90bf164b3556e
bakare <bakaredavid007@gmail.com>
2022-09-01 10:05:51 +0100

    test 1, task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    numerator = 0
+    deminator = 0
+    for i ,j in my_list:
+        numerator += (i * j)
+        deminator += j
+    return (numerator/deminator)
