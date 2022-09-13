832a29b5e4b7099f9f8798fae956d336f4469ea2
bakare <bakaredavid007@gmail.com>
2022-09-13 01:12:49 +0100

    :test 2, task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,4 +20,8 @@ def list_division(my_list_1, my_list_2, list_length):
             print("{}".format("division by 0"))
             new_list.append(0)
             index += 1
+        except Exception:
+            new_list.append(0)
+        finally:
+            pass
     return (new_list)
