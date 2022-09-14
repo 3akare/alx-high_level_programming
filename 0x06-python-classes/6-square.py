28b7ab34a28fecbfe5c6cb63fb772b7818abddd7
bakare <bakaredavid007@gmail.com>
2022-09-14 03:27:07 +0100

    test 8, task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,11 +34,10 @@ class Square:
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
-                value[0] < 0 or value[1] < 0 or
+                len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
-                len(value) != 2):
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-
         self.__position = value
 
     def area(self):
@@ -52,7 +51,7 @@ class Square:
 
     def my_print(self):
         """
-            prints out a square to stdout
+            prints out a square to stdout with character #
             Args:
                 self: an instance of Square
         """
