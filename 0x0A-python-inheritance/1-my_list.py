f35404d87f3b508d28fb9d25d9b6e077d5a276b9
bakare <bakaredavid007@gmail.com>
2022-09-26 20:45:54 +0100

    test 1, task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-my_list.py
+Desc: defines a class that inherits from list
+class name: Mylist
+"""
+
+
+class MyList(list):
+    """
+    Mylist: inherits from list object
+    """
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile('tests/1-my_list.txt', verbose=True)
