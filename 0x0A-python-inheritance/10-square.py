81fbf73cecb01dcc555f00666c4768692697ee0f
bakare <bakaredavid007@gmail.com>
2022-09-26 22:17:13 +0100

    test 1, task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('9-rectangle').Rectangle
+
+
+class Square(BaseGeometry):
+    """Represents a rectangle using BaseGeometry."""
+
+    def __init__(self, size):
+        """Intialize a new rectangle """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return the print() and str() representation of a rectangle"""
+        string = "[" + "Rectangle" + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,26 +2,23 @@
 """Defines a class Rectangle that inherits from BaseGeometry"""
 
 
-BaseGeometry = __import__('8-rectangle').BaseGeometry
+BaseGeometry = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
+class Square(BaseGeometry):
     """Represents a rectangle using BaseGeometry."""
 
-    def __init__(self, width, height):
+    def __init__(self, size):
         """Intialize a new rectangle """
-
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
+        super().integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         """Return the area of the rectangle"""
-        return self.__width * self.__height
+        return self.__size ** 2
 
     def __str__(self):
         """Return the print() and str() representation of a rectangle"""
         string = "[" + str(self.__class__.__name__) + "]"
-        string += str(self.__width) + "/" + str(self.__height)
+        string += str(self.__size) + "/" + str(self.__size)
         return string
