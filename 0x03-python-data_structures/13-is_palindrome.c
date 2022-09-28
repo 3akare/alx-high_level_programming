9fb0b41e6afb147ea60a5aa0051ce07e32b12198
bakare <bakaredavid007@gmail.com>
2022-09-28 12:07:53 +0100

    test 3, task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,5 @@
 #include "lists.h"
 /**
- * reverse_listint - reverses a linked list
- * @head: address of a linked lint
- *
- * Return: always succesful
- */
-
-listint_t *reverse_listint(listint_t **head)
-{
-
-	listint_t *prev = NULL, *next = NULL, *current;
-
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	current = *head;
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	*head = prev;
-	return (*head);
-}
-
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * desc- Technical interview practice
  * @head: a listint_t list
@@ -36,15 +9,31 @@ listint_t *reverse_listint(listint_t **head)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head, *temp2;
+	listint_t *temp = *head;
+	listint_t *temp2 = *head;
+	int len = 0, i = 0, eq = 0, *int_array, j = 0;
 
-	temp2 = reverse_listint(head);
-	while (temp != NULL && temp2 != NULL)
+	while (temp != NULL)
 	{
-		if (temp->n != temp2->n)
-			return (0);
-		temp = temp->next;
-		temp2 = temp2->next;
+		temp = (temp)->next;
+		len++;
 	}
-	return (1);
+	int_array = malloc(sizeof(int) * len);
+	while (temp2 != NULL)
+	{
+		int_array[i] = temp2->n;
+		temp2 = temp2->next;
+		i++;
+	}
+	for (j = 0; j < len; j++)
+	{
+		if (int_array[j] == int_array[len - j - 1])
+		{
+			eq++;
+		}
+	}
+	if (eq == len)
+		return (1);
+	return (0);
 }
+
