0e19e7e717a4f39dacb547943254b6c1921efaa3
bakare <bakaredavid007@gmail.com>
2022-10-03 23:20:15 +0100

    test 1, task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,7 @@ class Rectangle(Base):
             for j in range(self.width):
                 print('#', end='')
             print('')
+
+    def __str__(self):
+        text = "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"
+        return (text.format(self.id, self.x, self.y, self.width, self.height))
