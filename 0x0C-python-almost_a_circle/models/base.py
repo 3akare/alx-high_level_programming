e9167fe174d564d863132f91c4b04af6ccad2a80
bakare <bakaredavid007@gmail.com>
2022-10-03 22:03:27 +0100

    test 1, task 0 and 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a base model class"""
+
+
+class Base:
+    """
+    Represent the base model for all other classes in project 0x0C*
+    Attibutes:
+        __nb_objects (int): The nunmber of instantiated Bases
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+            Initialize a new Base
+
+            Args:
+                id(int): the indentity of a new Base
+        """
+        if (id is not None):
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
