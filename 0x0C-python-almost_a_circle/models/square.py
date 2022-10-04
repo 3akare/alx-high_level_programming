52f4a964400018c737ba9a9b49caed2763e42e1f
bakare <bakaredavid007@gmail.com>
2022-10-04 01:50:08 +0100

    test 1, task 13

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -70,3 +70,11 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """
+            Returns the dictionary representation of a Square
+        """
+        return dict(
+            {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        )
