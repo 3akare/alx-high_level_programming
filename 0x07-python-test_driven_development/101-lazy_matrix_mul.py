b0475f81cb23268234a403c2ab4c0e01e170fc9e
bakare <bakaredavid007@gmail.com>
2022-10-24 00:52:26 +0100

    101, test 2

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-import numpy as np
-"""
+'''
 Module Name: 101-lazy_matrix_mul.py
-Desc: multiples 2 matrices with numpy
+Desc: multiples 2 matrices using numpy
 Function Name: lazy_matrix_mul(m_a, m_b)
-"""
+'''
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    return(np.matmul(m_a, m_b))
+    '''
+    Multiples two matrices
+
+    Args:
+        m_a - first matrix
+        m_b - second matrix
+    Returns:
+        Multiplication of m_a and m_b with numpy
+    '''
+    return (np.matmul(m_a, m_b))
+
 
 if __name__ == '__main__':
     import doctest
