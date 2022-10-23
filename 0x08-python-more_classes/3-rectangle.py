6d173f0402ef04fcfb1edfdb1f00838a234f9ad1
bakare <bakaredavid007@gmail.com>
2022-10-23 21:37:07 +0100

    test 1, task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -23,26 +23,6 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    def area(self):
-        """
-            Area function
-            Args:
-                self: an instance of Rectangle
-            Return: the area of a rectangle object
-        """
-        return (self.__width * self.__height)
-
-    def perimeter(self):
-        """
-            perimeter function
-            Args:
-                self: an instance of Rectangle
-            Return: the perimeter of a rectangle object
-        """
-        if (self.__width == 0 and self.__height == 0):
-            return (0)
-        return (2 * (self.__width + self.__height))
-
     @property
     def height(self):
         return self.__height
@@ -51,8 +31,8 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif value < 0:
-            raise TypeError('height must be >= 0')
+        if value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     @property
@@ -63,18 +43,30 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif value < 0:
-            raise TypeError('width must be >= 0')
+        if value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
+    def area(self):
+        '''
+            area - returns the area of a rectangle instance
+        '''
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        '''
+            perimeter - returns the perimeter of a rectangle instance
+        '''
+        if ((self.__width == 0) or (self.__height == 0)):
+            return (0)
+        return (2 * (self.__width + self.__height))
+
     def __str__(self):
-        if (self.__height == 0 or self.__width == 0):
-            print("")
-        else:
-            for i in range(self.__height):
-                print("", end="")
-                for j in range(self.__width):
-                    print('#', end="")
-                if (i != self.__height - 1):
-                    print("")
+        if (self.__width == 0 or self.__height == 0):
+            return ('#')
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                print('#', end='')
+            if (self.__height - 1 != i):
+                print('')
         return ("")
