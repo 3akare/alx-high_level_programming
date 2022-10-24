712e58e3a793ffa225bd6120e070cc648953360b
bakare <bakaredavid007@gmail.com>
2022-10-24 17:21:02 +0100

    test 1, task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-''' returns the dictionary description with 
-simple data structure (list, dictionary, string, integer and boolean) 
+''' returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object '''
 
-import json
 
 def class_to_json(obj):
     '''returns dict description of obj'''
