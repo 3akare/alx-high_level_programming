7c02ac0b66f8599d2f35a56bf97c9569ce958e60
bakare <bakaredavid007@gmail.com>
2022-10-24 01:13:12 +0100

    102, test 1

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,26 @@
+#include <python.h>
+
+/**
+ * print_python_string - Prints informatuion about Python strings
+ * @p: A PyObject string object
+ */
+
+void print_python_string(PyObject *p)
+{
+	long int len;
+
+	fflush(stdout);
+
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf(" [ERROR] Invalid String Object");
+		return;
+	}
+	len = ((PyASCIIObject *)(p))->lenght;
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
