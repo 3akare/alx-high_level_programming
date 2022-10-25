9d59672c1d5f8a01cb58a7965f989bcce072bfe7
bakare <bakaredavid007@gmail.com>
2022-10-25 23:09:54 +0100

    test 3, task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,13 +8,12 @@ def append_after(filename='', search_string='', new_string=''):
 
     with open(filename, 'r', encoding='utf-8') as f:
         string = f.readlines()
-
-    i = 0
-    while i < len(string):
-        if search_string in string[i]:
-            string[i:i + 1] = [string[i], new_string]
+        i = 0
+        while i < len(string):
+            if search_string in string[i]:
+                string[i:i + 1] = [string[i], new_string]
+                i += 1
             i += 1
-        i += 1
 
     with open(filename, 'w', encoding='utf-8') as f:
-        f.writelines(list_string)
+        f.writelines(string)
