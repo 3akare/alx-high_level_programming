0f98d5cad8681569cae89f370affc722646e8c4e
bakare <bakaredavid007@gmail.com>
2022-10-26 00:55:08 +0100

    test 3, task 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
 '''script that reads stdin line by line and computes metrics'''
-import sys
+if __name__ == '__main__':
+    import sys
 
-o_put = '''File size: {}'''
-t_size = 0
-num = 10
-s_code = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-    }
-try:
-    for ln in sys.stdin:
-        if (num == 0):
-            print(o_put.format(t_size))
-            for k, v in s_code.items():
-                if v != 0:
-                    print(f'{k}: {v}')
-            num = 10
-        num -= 1
-        ln = [ln.split()]
-        t_size += int(ln[0][-1])
-        s_code[ln[0][-2]] += 1
-except KeyboardInterrupt:
-    print(o_put.format(t_size))
-    for k, v in s_code.items():
-        if v != 0:
-            print(f'{k}: {v}')
-
+    o_put = '''File size: {}'''
+    t_size = 0
+    num = 10
+    s_code = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
+    try:
+        for ln in sys.stdin:
+            if (num == 1):
+                print(o_put.format(t_size))
+                for k, v in s_code.items():
+                    if v != 0:
+                        print(f'{k}: {v}')
+                num = 10
+            num -= 1
+            ln = [ln.split()]
+            t_size += int(ln[0][-1])
+            s_code[ln[0][-2]] += 1
+    except KeyboardInterrupt:
+        print(o_put.format(t_size))
+        for k, v in s_code.items():
+            if v != 0:
+                print(f'{k}: {v}')
