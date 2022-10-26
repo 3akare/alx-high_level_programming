8b8f5e2cd28e8cf055b2747cb71dd80524cd7d06
bakare <bakaredavid007@gmail.com>
2022-10-26 02:53:11 +0100

    test 1, task models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return ('[]')
         return json.dumps(list_dictionaries)
-    
+
     @staticmethod
     def from_json_string(json_string):
         """
@@ -46,7 +46,6 @@ class Base:
         if (json_string is None or json_string == []):
             return ([])
         return json.loads(json_string)
-
 
     @classmethod
     def save_to_file(cls, list_objs):
