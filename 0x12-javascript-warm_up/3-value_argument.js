429f1596c9921c4734dae62989c8b83640087396
bakare <bakaredavid007@gmail.com>
2022-11-29 00:27:27 +0100

    test 3, task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 let index = 0;
-if (process.argv.length <= 2) { console.log('No argument'); } else {
-  process.argv.forEach(element => {
-    if (index === 2) {
-      console.log(element);
-    }
-    index++;
-  });
+
+while (process.argv[index]) {
+  if (index === 2) {
+    console.log(process.argv[index]);
+  }
+  index++;
 }
