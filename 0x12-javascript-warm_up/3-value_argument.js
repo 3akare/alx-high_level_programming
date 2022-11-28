75c6e500da7f55c6806dec6bcb771edc6fe1f7f9
bakare <bakaredavid007@gmail.com>
2022-11-29 00:02:43 +0100

    typo fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 2) { console.log('No argument'); } else {
-  for (let i = 2; i < process.argv.length; i++) { console.log(process.argv[i]); }
+  console.log(process.argv[3]);
 }
