0eca8ddf15633e8548a6d73e7abf2d00246aa980
bakare <bakaredavid007@gmail.com>
2022-11-30 03:40:15 +0100

    test 1, task 100

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const list = require('./100-data.js').list;
+const newList = list.map(indexFunc);
+function indexFunc (num) {
+  return (num * list.indexOf(num));
+}
+
+console.log(newList);
