a534bc2c07a669d7f9d66dcdbf39776c638e3154
bakare <bakaredavid007@gmail.com>
2022-12-01 13:19:41 +0100

    test 2, task 100 and 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const fs = require("fs")
-var string;
+const fs = require('fs');
+let string;
 
-fs.readFile(process.argv[2], (err, data) =>{
-    if (err) throw err;
-    string += data.toString();
-})
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) throw err;
+  string += data.toString();
+});
 
-console.log(string)
+console.log(string);
 // fs.readFile("./main.js", (err, data) =>{
 //     if (err) throw err;
 //     console.log(data.toString());
