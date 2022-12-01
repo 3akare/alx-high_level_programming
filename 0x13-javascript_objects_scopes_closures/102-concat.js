db9c977e3c407ce678a387b20178d112fbcce82a
bakare <bakaredavid007@gmail.com>
2022-12-01 13:55:29 +0100

    test 2, task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data1) => {
-  if (err) { console.error(err); }
+  if (err) { data1 = ''; }
   fs.readFile(process.argv[3], 'utf8', (err, data2) => {
-    if (err) { console.error(err); }
-    const data3 = `${data1.slice(0, -1)}\n${data2.slice(0, -1)}\n`;
+    if (err) { data2 = ''; }
+    const data3 = `${data1}${data2}`;
     fs.writeFile(process.argv[4], data3, (err) => {
       if (err) throw err;
     });
