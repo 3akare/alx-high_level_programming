341664a3aee40919fea098e4e476b704b358d843
bakare <bakaredavid007@gmail.com>
2022-12-01 14:27:00 +0100

    test 2, task 5 and 6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,25 +1,9 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
-  }
-
-  print () {
-    const array = [];
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        array.push('X');
-      }
-      console.log(array.join(''));
-      array.length = 0;
-    }
-  }
-
-  double () {
-    this.size *= 2;
   }
 };
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,21 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
-  }
-
-  print () {
-    const array = [];
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        array.push('X');
-      }
-      console.log(array.join(''));
-      array.length = 0;
-    }
-  }
-
-  double () {
-    this.size *= 2;
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      const array = [];
-      for (let i = 0; i < this.size; i++) {
-        for (let j = 0; j < this.size; j++) {
-          array.push(c);
-        }
-        console.log(array.join(''));
-        array.length = 0;
+    if (c === undefined) { c = 'X'; }
+    const array = [];
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
+        array.push(c);
       }
+      console.log(array.join(''));
+      array.length = 0;
     }
   }
 };
