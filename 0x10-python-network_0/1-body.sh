82403b71db13b18ceb1fe1fbcb1349ff2914236f
David Bakare <bakaredavid007@gmail.com>
2022-12-17 23:51:34 +0100

    test 1 ,task 0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a GET request to the URL, and displays the body of the response
-size=$(curl -sI ${1} | grep "HTTP/1.1" | cut -d " " -f2)
-if [ $size -eq 200 ]
-then
-    curl -sI ${1}
-fi
+curl -sL "$1"
