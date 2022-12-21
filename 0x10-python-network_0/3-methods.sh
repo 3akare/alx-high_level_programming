6417d3a6cdbbacd26f5ac00ccd116c104da07755
David Bakare <bakaredavid007@gmail.com>
2022-12-21 02:23:21 +0100

    test 1, task 3 and 5

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.. -w is the write out option. It writes out %{http_code}\n which is a special variable. For More special variables check (https://everything.curl.dev/usingcurl/verbose/writeout)
-curl -sLw "%{method}" -o /dev/null "$1"
+curl -sI "$1" | grep "Allow" | cut -d " "  -f2
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Takes in URL, send a POST request to the passed URL, and displays the body of the response
-curl -X POST "$1" -d'{"email" : test@gmail.com, "subject" : I will always be here for PLD}'
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
