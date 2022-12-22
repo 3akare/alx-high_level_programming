813885b42de44bf27800fa25d081c2092fdfb5c6
3akare <bakaredavid007@gmail.com>
2022-12-22 13:53:30 +0000

    python network, task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.. -w is the write out option. It writes out %{http_code}\n which is a special variable. For More special variables check (https://everything.curl.dev/usingcurl/verbose/writeout)
-curl -sI "$1" | grep "Allow" | cut -d " "  -f2
+curl -sI "$1" | grep "Allow" | cut -d " "  -f2-
