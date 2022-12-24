ceadb4d18fc580aef534623520d900eb3e0df541
3akare <bakaredavid007@gmail.com>
2022-12-24 00:42:58 +0000

    python network, test 2, task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,18 @@
 '''Takes in URL, send a request to the URL and displays the value of
 the X-Request-Id varibale found in the header of the response'''
 
-
-import requests
+import urllib.request
 import sys
 
-with requests.get(sys.argv[1]) as req:
-    print(req.headers.get('X-Request-Id'))
+with urllib.request.urlopen(sys.argv[1]) as response:
+    location = response.headers.__dict__.get('_headers')
+    for i in location:
+        if 'X-Request-Id' in list(i):
+            print(i[1])
+
+# Solution with request module
+# import requests
+# import sys
+
+# with requests.get(sys.argv[1]) as req:
+#     print(req.headers.get('X-Request-Id'))
