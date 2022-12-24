237063f934b9b0eb1a69316e50f0a4a20ca9a4cf
3akare <bakaredavid007@gmail.com>
2022-12-24 16:38:22 +0000

    python network, test 1, task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''A script that takes 2 arguments n order to solve this challenge'''
+
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    URL = 'https://api.github.com/repos/{}/{}/commits'
+    with requests.get(URL.format(sys.argv[1], sys.argv[2])) as req:
+        for i in range(0, 10):
+            name = req.json()[i].get('commit').get('tree').get('sha')
+            sha = req.json()[i].get('commit').get('author').get('name')
+            print('{}: {}'.format(name, sha))
