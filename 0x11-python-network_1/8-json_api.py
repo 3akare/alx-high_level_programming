c4d5d0b78c5eea6af03014661f279103b626b011
3akare <bakaredavid007@gmail.com>
2022-12-24 03:10:46 +0000

    python network test 1, task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Takes in a letter and sends a POST requestr to http://0.0.0.0:5000/search_user'''
+'''Takes in a letter and sends a POST requestr to
+http://0.0.0.0:5000/search_user'''
 
 import sys
 import requests
@@ -7,8 +8,9 @@ import requests
 if __name__ == '__main__':
     try:
         data = {'q', sys.argv[1]}
-        with requests.post('http://0.0.0.0:5000/search_user', data). as req:
-            print(req.text)
+        with requests.post('http://0.0.0.0:5000/search_user', data) as req:
+            try:
+                response = r.json
+                print(f'[{response.get('id')}], {response.get('name')}')
     except IndexError:
         print('No result')
-    
