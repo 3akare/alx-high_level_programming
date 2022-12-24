81d6e117902772bd5216a105cc6a95fc0d8a3279
3akare <bakaredavid007@gmail.com>
2022-12-24 03:23:29 +0000

    python network test 1, task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    letter = "" if len(sys.argv[1]) == 1 else sys.argv[1]
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     data = {'q': letter}
     try:
         with requests.post('http://0.0.0.0:5000/search_user', data) as req:
