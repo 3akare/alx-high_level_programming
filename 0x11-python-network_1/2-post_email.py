be29f58b6b1676a669fe405f8f52b035d7f0b6b2
3akare <bakaredavid007@gmail.com>
2022-12-24 01:08:41 +0000

    python network, test 2, task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,9 +8,12 @@ import urllib.request
 import urllib.parse
 import sys
 
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(f'Your email is: {sys.argv[2]}')
+try:
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(f'Your email is: {sys.argv[2]}')
+except Exception:
+    pass
