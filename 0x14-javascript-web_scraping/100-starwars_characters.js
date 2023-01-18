3a2e8e96fa7518e807f0c0a0d946d4f85879b248
3akare <bakaredavid007@gmail.com>
2023-01-18 06:45:38 +0000

    test 1, task 101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,36 +1,15 @@
 #!/usr/bin/node
 // Prints all charcters of a star wars movie
+
 const request = require('request');
 
-async function getData (filmId) {
-  let filmResponse;
-  try {
-    filmResponse = await new Promise((resolve, reject) => {
-      request(`https://swapi-api.alx-tools.com/api/films/${filmId}`, (err, response, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve({ response, body });
-        }
-      });
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) { console.log(response.statusCode); throw err; }
+  const res = JSON.parse(body).characters;
+  res.forEach(element => {
+    request(element, (err, response, body) => {
+      if (err) { throw err; }
+      console.log(JSON.parse(body).name);
     });
-    const characters = JSON.parse(filmResponse.body).characters;
-    for (const character of characters) {
-      const characterResponse = await new Promise((resolve, reject) => {
-        request(character, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve({ response, body });
-          }
-        });
-      });
-      console.log(JSON.parse(characterResponse.body).name);
-    }
-  } catch (error) {
-    console.log(filmResponse.response.statusCode);
-    throw error;
-  }
-}
-
-getData(process.argv[2]);
+  });
+});
