d1a0044b1fa0ca313fcdd2e3d7f8e6f55baa6dc3
David Bakare <bakaredavid007@gmail.com>
2023-01-26 18:03:30 +0100

    Upload for initial jQuery

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,9 @@
+$('DIV#clear_list').on('click', () => {
+  $('li').remove();
+});
+$('DIV#add_item').on('click', () => {
+  $('UL.my_list').append('<li>Item</li>');
+});
+$('DIV#remove_item').on('click', () => {
+  $('UL.my_list li:last-child').remove();
+});
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,17 @@
-$('document').ready(function () {
-	$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-	  $('DIV#hello').text(data.hello);
-	});
-  });
+const settings = {
+  cache: false,
+  dataType: 'json',
+  async: true,
+  crossDomain: true,
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*'
+  }
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
