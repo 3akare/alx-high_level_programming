2b25beb26f1ac685e73745f2db250f41d9702db9
David Bakare <bakaredavid007@gmail.com>
2023-01-30 16:27:17 +0100

    Keyboard Interrupt Update

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -6,3 +6,13 @@ $(document).ready(() => {
     });
   });
 });
+$(document).ready(() => {
+  $('#language_code').keypress(event => {
+    if (event.which === 13){
+      const lang = $('INPUT#language_code').val();
+      $.get(`https://hellosalut.stefanbohacek.dev/?lang=${lang}`, function (data) {
+        $('DIV#hello').text(data.hello);
+      });
+    }
+  });
+})
